3ff035aa6e42966b809f8b37d5ba45245d58d0b2
Glitch (bramble-woolen-pyramid) <none>
2020-12-02 02:56:58 +0000

    🚎🚍 Updated with Glitch

--- index.js
+++ index.js
@@ -1,62 +1,84 @@
-const { Client, MessageEmbed } = require("discord.js");
+const { Client, Collection, Intents , MessageEmbed} = require("discord.js");
+const { readdirSync } = require("fs");
 const { Manager } = require("erela.js");
-
-const client = new Client();
+const Spotify = require("erela.js-spotify");
+const handler = require("@tomdev/discord.js-command-handler")
+const durasi = require("humanize-duration");
+const client = new Client({
+    disableMentions: "everyone",
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+    ws: { intents: Intents.ALL }
+});
 client.config = require("./config.json");
-const formatTime = require("./util/time.js");
+client.logger = require("./util/logger.js");
+
+const { clientID, clientSecret } = require("./config.json");
 
 var prefix = client.config.prefix;
-var handler = require("@tomdev/discord.js-command-handler");
 var cmdhandler = new handler(client, "/commands", prefix);
+
 
 client.manager = new Manager({
   nodes: [
     {
       host: "localhost",
       port: 2333,
-      password: "password",
+      password: "youshallnotpass",
       retryDelay: 5000
     }
   ],
-  autoplay: true,
-  send(id, payload) {
+  send: (id, payload) => {
     const guild = client.guilds.cache.get(id);
     if (guild) guild.shard.send(payload);
-  }
+  },
+  autoPlay: true,
+  plugins: [
+    new Spotify({
+      clientID,
+      clientSecret
+    })
+  ]
 })
-  .on("nodeConnect", node =>
-    console.log(`Node ${node.options.identifier} connected`)
-  )
-  .on("nodeError", (node, error) =>
-    console.log(
-      `Node ${node.options.identifier} had an error: ${error.message}`
-    )
-  )
+  .on("nodeConnect", node => console.log(`Node "${node.options.identifier}" connected.`))
+  .on("nodeError", (node, error) => console.log(
+    `Node "${node.options.identifier}" encountered an error: ${error.message}.`
+  ))
   .on("trackStart", (player, track) => {
-    const user = client.users.cache.get(track.requester.id);
-    const embed = new MessageEmbed()
-      .setAuthor(
-        track.requester.username,
-        user.displayAvatarURL({ dynamic: true })
-      )
-      .setTitle("Now Playing")
-      .setColor("GREEN")
-      .setThumbnail(`https://i.ytimg.com/vi/${track.identifier}/hqdefault.jpg`)
-      .setDescription(
-        `:musical_note: ${
-          track.title
-        },
-          true
-        )}**`
-      )
-      .setTimestamp();
-    client.channels.cache.get(player.textChannel).send({ embed: embed });
+  const start = new MessageEmbed()
+        .setDescription(`🎶 **Started Playing**\n [${track.title}](${track.uri}) - \`[${durasi(track.duration, {largest: 2 })}]\``)
+        .setThumbnail(track.displayThumbnail("hqdefault"))
+        .setColor("BLACK")
+        .setTimestamp()
+        .setFooter(`Request by: ${track.requester.tag}`, track.requester.displayAvatarURL());
+    
+    const channel = client.channels.cache.get(player.textChannel);
+    channel.send({embed: start})
   })
   .on("queueEnd", player => {
-    client.channels.cache.get(player.textChannel).send("Queue has ended.");
-
+    const channel = client.channels.cache.get(player.textChannel);
+    channel.send("Queue has ended.");
     player.destroy();
   });
+
+client.on("disconnect", () =>
+  client.logger.log("Bot is disconnecting...", "warn")
+);
+client.on("reconnecting", () =>
+  client.logger.log("Bot reconnecting...", "log")
+);
+client.on("warn", error => client.logger.log(error, "warn"));
+client.on("error", error => {
+  client.logger.log(error, "error");
+  console.error(error);
+});
+process.on("unhandledRejection", error => {
+  client.logger.log("Unhandled promise rejection:" + error, "error");
+  console.error("Unhandled promise rejection:", error);
+});
+process.on("uncaughtException", error => {
+  client.logger.log("Unhandled promise exception:" + error, "error");
+  console.error("Unhandled promise exception:", error);
+});
 
 client.once("ready", () => {
   console.log("I am ready!");
@@ -67,6 +89,13 @@ client.on("raw", d => client.manager.updateVoiceState(d));
 
 client.on("message", async message => {
   cmdhandler.handleCommand(message);
+});
+
+readdirSync("./events/").forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.logger.log(`Loading Events Client ${eventName}`, "event");
+    client.on(eventName, event.bind(null, client));
 });
 
 client.login(client.config.token);
