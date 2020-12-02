48dd69a40d935c6c0f5e620182184aa7eba141cd
sweetbutcrazy <71241176+sweetbutcrazy@users.noreply.github.com>
2020-12-02 17:00:48 +0700

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -94,8 +94,24 @@ module.exports = {
         .setDescription(results) 
         .setFooter(`<a:60sec:783543097790824479> ${client.user.username} ~ Gang Sebelah © 2020`)
         message.channel.send({ embed: mbd });
+        const collector = message.channel.createMessageCollector(m => {
+            return m.author.id === message.author.id && new RegExp('^([1-5|cancel])$', "i").test(m.content);
+         }, { time: 60000, max: 1 });
+          collector.on("collect", m => {
+          if(/cancel/i.test(m.content)) return collector.stop("cancelled");
 
-        try {
+           const track = tracks[Number(m.content) - 1];
+           player.queue.add(track);
+           if(getPlayer.queue[0]) message.channel.send(`Enqueuing track **${track.title}**.`);
+           if (!player.playing) player.play();
+           message.channel.messages.fetch({ limit: 2 }).then(messages => {
+           message.channel.bulkDelete(messages)
+           });
+         });
+           collector.on("end", (_, reason) => {
+            if(["time", "cancelled"].includes(reason)) return message.channel.send("Cancelled selection");
+      });
+        /*try {
           collected = await message.channel.awaitMessages(filter, {
             max: 1,
             time: 60000,
@@ -129,6 +145,6 @@ module.exports = {
         if (!player.playing && !player.paused && !player.queue.size)
           player.play();
         return message.reply(`enqueuing \`${track.title}\`.`);
-    }
+    }*/
   }
 };
