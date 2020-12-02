a1d8cf17027aafc126f11f3db5d73302af239d32
sweetbutcrazy <71241176+sweetbutcrazy@users.noreply.github.com>
2020-12-02 18:10:39 +0700

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -21,7 +21,7 @@ module.exports = {
         embed: {
           color: "#D70FB6",
           description:
-            "**example: \nn.play ncs \nn.play https://www.youtube.com/watch?v=Nxs_mpWt2BA**"
+            "**usage: \n``n.play ncs \nn.play https://www.youtube.com/watch?v=Nxs_mpWt2BA``**"
         }
       });
 
@@ -32,7 +32,7 @@ module.exports = {
     });
 
     if (player.state !== "CONNECTED") player.connect();
-
+    player.set("autoplay", false) 
     const search = args.join(" ");
     let res;
 
@@ -52,85 +52,44 @@ module.exports = {
       case "NO_MATCHES":
         if (!player.queue.current) player.destroy();
         return message.reply("there were no results found.");
-      case "TRACK_LOADED":
-        player.queue.add(res.tracks[0]);
-
-        if (!player.playing && !player.paused && !player.queue.size)
-          player.play();
-        return message.channel.send({
-          embed: {
-            color: "0F1621",
-            description: `enqueuing \`${res.tracks[0].title}\`.`
-          }
-        });
-      case "PLAYLIST_LOADED":
-        player.queue.add(res.tracks);
-
-        if (
-          !player.playing &&
-          !player.paused &&
-          player.queue.totalSize === res.tracks.length
-        )
-          player.play();
-        return message.channel.send({
-          embed: {
-            color: "#0F1621",
-            description: `enqueuing playlist \`${res.playlist.name}\` with ${res.tracks.length} tracks.`
-          }
-        });
-      case "SEARCH_RESULT":
-        let max = 5,
-          collected,
-          filter = m =>
-            m.author.id === message.author.id && /^(\d+|end)$/i.test(m.content);
-        if (res.tracks.length < max) max = res.tracks.length;
-
-        const results = res.tracks
-          .slice(0, max)
-          .map((track, index) => `${++index} - \`${track.title}\``)
-          .join("\n");
-        const mbd = new MessageEmbed() 
-        .setColor("RED")
-        .setAuthor(`${message.guild.name}`, `https://cdn.discordapp.com/emojis/783049850996523048.gif`)
-        .setDescription(results) 
-        .setFooter(`${client.user.username} ~ Gang Sebelah © 2020`) 
-        message.channel.send({ embed: mbd });
-      
-        try {
-          collected = await message.channel.awaitMessages(filter, {
-            max: 1,
-            time: 60000,
-            errors: ["time"]
-          });
-        } catch (e) {
-          if (!player.queue.current) player.destroy();
-          return message.channel
-            .send("**You didn't provide a selection.**")
-            .then(m => m.delete({ timeout: 5000 }));
+            case 'TRACK_LOADED':
+                var track = res.tracks[0];
+                player.queue.add(track);
+                if (!player.playing && !player.paused && !player.queue.size) { 
+                    return player.play();
+                } else {
+                    var thing = new MessageEmbed()
+                        .setColor("#D70FB6")
+                        .setTimestamp()
+                        .setThumbnail(track.displayThumbnail("hqdefault"))
+                        .setDescription(`${emojiaddsong} **Song added to queue **\n[${track.title}](${track.uri}) - \`[${durasi}]\``)
+                        .setFooter(`Request by: ${message.author.tag}`, message.author.displayAvatarURL());
+                    return message.channel.send(thing);
+                }
+            case 'PLAYLIST_LOADED':
+                player.queue.add(res.tracks);
+                if (!player.playing && !player.paused && player.queue.totalSize === res.tracks.length) player.play();
+                var thing = new MessageEmbed()
+                    .setColor("#D70FB6")
+                    .setTimestamp()
+                    .setThumbnail(res.tracks[0].displayThumbnail("hqdefault"))
+                    .setDescription(`${emojiplaylist} **Playlist added to queue **\n${res.tracks.length} Songs **${res.playlist.name}** - \`[${durasi}]\``)
+                    .setFooter(`Request by: ${message.author.tag}`, message.author.displayAvatarURL());
+                return message.channel.send(thing);
+            case 'SEARCH_RESULT':
+                var track = res.tracks[0];
+                player.queue.add(track);
+                if (!player.playing && !player.paused && !player.queue.size) {
+                    return player.play();
+                } else {
+                    var thing = new MessageEmbed()
+                        .setColor("#D70FB6")
+                        .setTimestamp()
+                        .setThumbnail(track.displayThumbnail("hqdefault"))
+                        .setDescription(` **Songs added to queue **\n[${track.title}](${track.uri}) - \`[${durasi}]\``)
+                        .setFooter(`Request by: ${message.author.tag}`, message.author.displayAvatarURL());
+                    return message.channel.send(thing);
+                }
         }
-
-        const first = collected.first().content;
-
-        if (first.toLowerCase() === "end") {
-          if (!player.queue.current) player.destroy();
-          return message.channel
-            .send("Cancelled selection.")
-            .then(m => m.delete({ timeout: 5000 }));
-        }
-
-        const index = Number(first) - 1;
-        if (index < 0 || index > max - 1)
-          return message.reply(
-            `the number you provided too small or too big (1-${max}).`
-          ).then(m => m.delete({ timeout: 5000 }));
-
-        const track = res.tracks[index];
-        player.queue.add(track);
-
-        if (!player.playing && !player.paused && !player.queue.size)
-          player.play();
-        return message.reply(`enqueuing \`${track.title}\`.`);
-
     }
-  }
-};
+}
