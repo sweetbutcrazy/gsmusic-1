4ff4814b8a84de41083657ff1ec44c89989bae3a
sweetbutcrazy <71241176+sweetbutcrazy@users.noreply.github.com>
2020-12-05 09:42:43 +0700

    Create skip.js

---
+++ commands/skip.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: "skip",
+    aliases: [], 
+    description: "This is an skip command", 
+    usage: "skip", 
+    run: async function(client, args, message ) => { 
+      const player = message.client.manager.get(message.guild.id);
+      if (!player) return message.reply("there is no player for this guild.");
+  
+      const { channel } = message.member.voice;
+      if (!channel) return message.reply("you need to join a voice channel.");
+      if (channel.id !== player.voiceChannel) return message.reply("you're not in the same voice channel.");
+
+      if (!player.queue.current) return message.reply("there is no music playing.")
+
+      const { title } = player.queue.current;
+
+      player.stop();
+      return message.reply(`${title} was skipped.`)
+    }
+  } 
