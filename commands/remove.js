844b5766e99a5444d8a0f1cc87c33634d88bf0d7
sweetbutcrazy <71241176+sweetbutcrazy@users.noreply.github.com>
2020-12-05 19:04:59 +0700

    Create remove.js

---
+++ commands/remove.js
@@ -0,0 +1,62 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "example", //neded
+    aliases: ["e", "exa"],
+    category: "Main",
+    description: "This is an example command",
+    usage: "!example",
+    run: async function (client, command, args, message) { //needed
+        const { channel } = message.member.voice;
+		const player = message.client.manager.get(message.guild.id);
+
+		if (!channel) {
+            let thing = new MessageEmbed()
+                .setColor(client.color)
+                .setDescription("You need to join a voice channel!");
+            return message.channel.send(thing);
+        }
+        
+        if (!player) {
+            let thing = new MessageEmbed()
+                .setColor(client.color)
+                .setDescription("There is no player for this guild.");
+            return message.channel.send(thing);	
+        }
+        
+        if (channel.id !== player.voiceChannel) {
+            let thing = new MessageEmbed()
+                .setColor(client.color)
+                .setDescription(`You must be in the same channel as ${client.user}`);
+            return message.channel.send(thing);
+        }
+
+        if (!player.queue.current) {
+            let thing = new MessageEmbed()
+                .setColor(client.color)
+                .setDescription("There is no music playing.");
+            return message.channel.send(thing);
+        }
+
+        const position = (Number(args[0]) - 1);
+        if (position > player.queue.size) {
+            const number = (position + 1);
+            let thing = new MessageEmbed()
+                .setColor(client.color)
+                .setDescription(`No songs at number ${number}.\nTotal Songs: ${player.queue.size}`);
+            return message.channel.send(thing);
+        }
+
+		const song = player.queue.remove(position);
+
+		const emojieject = "<:remove:784748892109275196>";
+
+		let thing = new MessageEmbed()
+			.setColor(client.color)
+			.setTimestamp()
+			.setDescription(`${emojieject} Removed\n[${song.track.title}](${song.track.uri})`)
+			.setFooter(`Request by: ${message.author.tag}`, message.author.displayAvatarURL());
+	  return message.channel.send(thing);
+	
+  	}
+};
