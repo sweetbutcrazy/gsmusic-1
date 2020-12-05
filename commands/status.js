b96877b4070a2eb93ca3ab0f22e0d6901a8e38a0
sweetbutcrazy <71241176+sweetbutcrazy@users.noreply.github.com>
2020-12-05 13:20:23 +0700

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -18,14 +18,15 @@ module.exports = {
             .setThumbnail(message.client.user.displayAvatarURL())
             .setFooter(`${message.client.user.username} ~ Gang Sebelah © 2020`)
             .setAuthor("STATISTIC", "https://cdn.discordapp.com/emojis/588679397909987350.png", "https://discord.gg/gangsebelah") 
-            .setDescription(`
+            .addField("**⦊ GENERAL :**", `
+**• Username: ${client.user.tag}
 **• Servers** : ${message.client.guilds.cache.size.toLocaleString()}
 **• Channels** : ${message.client.channels.cache.size.toLocaleString()}
 **• Users** : ${message.client.users.cache.size.toLocaleString()}
 **• Discord.js** : v${version}
 **• Node** : ${process.version}
-
-**• SYSTEM :**
+`)
+           .addField(**⦊ SYSTEM :**, `
 **• Platfrom** : \`${os.type}\`
 **• Uptime** : \`${duration1}\`
 **• CPU** :
@@ -38,6 +39,7 @@ module.exports = {
 > **• Free Memory** : \`${(os.freemem() / 1024 / 1024).toFixed(2)} Mbps\`
 > **• Heap Total** : \`${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)} Mbps\`
 > **• Heap Usage** : \`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} Mbps\`
+
 `);
         message.channel.send({embed: embed});
     }
